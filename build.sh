8909a26bdf6aeab76aa9163f9e3aa3bf7a261d37
ggdoe <gregoire.doebele@ens-paris-saclay.fr>
2024-08-25 17:46:18 +0200

    name for the bin

--- build.sh
+++ build.sh
@@ -1,12 +1,15 @@
 #!/bin/sh
 
-# CFLAGS="-g -ggdb -flto -O3"
 # CFLAGS="-flto -Ofast"
 CFLAGS="-flto -O3"
 WFLAGS="-Wall -Wextra -pedantic"
 LFLAGS="-fopenmp"
 
+output_name=fr
+install_cmd="ln -s $PWD/$output_name ~/.local/bin/$output_name"
+# echo install command: $install_cmd
+
 SRC="main.c findrecent.c sortmerge.c entry.c buffer.c parse_options.c launch_fzf.c"
 
 set -xe
-gcc $WFLAGS $CFLAGS $SRC $LFLAGS
+gcc $WFLAGS $CFLAGS $SRC $LFLAGS -o $output_name
