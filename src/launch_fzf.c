75e2a1f44ba5c4fc1b0d557f267273794541a480
ggdoe <gregoire.doebele@ens-paris-saclay.fr>
2026-05-24 18:38:03 +0200

    fzf-exec: print full command

--- src/launch_fzf.c
+++ src/launch_fzf.c
@@ -128,17 +128,18 @@ void fill_select_cmd(char* select, struct options *options)
         "| " FZF_CMD " "
         "--header \"Enter a command, \\`%\\` is substituted by the filepath. \" --header-first "
         "--bind=enter:become:'"
-        "printf -v file \"%q\" \\{}; "             // espace the filename
+        "printf -v file \"%q\" \\{}; "                             // espace the filename
         "cmd=\"$FZF_QUERY\"; "
-        "[ -z \"${FZF_QUERY// }\" ] && cmd=echo; " // if empty command, use echo
-        "cmd=${cmd//%%/\x1f}; "                    // escape %%
-        "if [ -z \"${cmd//[^%]}\" ]; then "        // if no % in command
-          "cmd=\"$cmd $file\"; "                   // append the filename
+        "[ -z \"${FZF_QUERY// }\" ] && cmd=echo; "                 // if empty command, use echo
+        "cmd=${cmd//%%/\x1f}; "                                    // escape %%
+        "if [ -z \"${cmd//[^%]}\" ]; then "                        // if no % in command
+          "cmd=\"$cmd $file\"; "                                   // append the filename
         "else "
-          "cmd=\"${cmd//%/$file}\"; "              // substitute % by filename
+          "cmd=\"${cmd//%/$file}\"; "                              // substitute % by filename
         "fi; "
-        "cmd=\"${cmd//\x1f/%}\"; "                 // restore escaped %%
-        "eval \"$cmd\"; "                          // execute command
+        "cmd=\"${cmd//\x1f/%}\"; "                                 // restore escaped %%
+        "[ -z \"${FZF_QUERY// }\" ] || echo \"\\$ $cmd\" >&2; "    // print command in stderr
+        "eval \"$cmd\"; "                                          // execute command
         "' "
         "--disabled --height 5 --info hidden --no-separator --no-scrollbar "
         "--layout reverse --border --margin 1,5% --padding=1 --pointer \"\" "
